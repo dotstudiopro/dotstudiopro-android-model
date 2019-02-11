7de639824086b36e04b31dc7a8e5ad107b47dadc
Mohsin <mohsin@dotstudioz.com>
2019-02-11 12:59:59 +0530

    updating the hascode of spotlightchannel dto to use slug instead of the id

--- model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/SpotLightChannelDTO.java
+++ model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/SpotLightChannelDTO.java
@@ -306,17 +306,24 @@ public class SpotLightChannelDTO implements Serializable {
 
     @Override
     public boolean equals(Object o) {
+        /*if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SpotLightChannelDTO that = (SpotLightChannelDTO) o;
+
+        return id.equals(that.id);*/
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         SpotLightChannelDTO that = (SpotLightChannelDTO) o;
 
-        return id.equals(that.id);
+        return slug.equals(that.slug);
 
     }
 
     @Override
     public int hashCode() {
-        return id.hashCode();
+        //return id.hashCode();
+        return slug.hashCode();
     }
 }
