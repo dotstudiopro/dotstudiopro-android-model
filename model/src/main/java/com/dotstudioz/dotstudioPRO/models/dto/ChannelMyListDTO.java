7cc5e5092afccb87267bb0046facf6a7119d3c6e
Mohsin <mohsin@dotstudioz.com>
2019-03-07 04:13:23 +0530

    Updated ChannelMyListDTO by adding categoriesArrayList to hold string of categories id also added parentChanneMyListDTO to same DTO

--- model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/ChannelMyListDTO.java
+++ model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/ChannelMyListDTO.java
@@ -13,7 +13,7 @@ public class ChannelMyListDTO {
     private String categoryName;
     private boolean isProduct;
     private ArrayList<String> categoriesArrayList = new ArrayList<>();
-    private ChannelMyListDTO parentChannelMyListDTO = new ChannelMyListDTO();
+    private ParentChannelMyListDTO parentChannelMyListDTO = new ParentChannelMyListDTO();
 
     private String parentCategorySlug;
 
@@ -103,11 +103,95 @@ public class ChannelMyListDTO {
         this.categoriesArrayList = categoriesArrayList;
     }
 
-    public ChannelMyListDTO getParentChannelMyListDTO() {
+    public ParentChannelMyListDTO getParentChannelMyListDTO() {
         return parentChannelMyListDTO;
     }
 
-    public void setParentChannelMyListDTO(ChannelMyListDTO parentChannelMyListDTO) {
+    public void setParentChannelMyListDTO(ParentChannelMyListDTO parentChannelMyListDTO) {
         this.parentChannelMyListDTO = parentChannelMyListDTO;
+    }
+
+    public class ParentChannelMyListDTO {
+        private String id;
+        private String title;
+        private String companyId;
+        private String poster;
+        private String spotlightPoster;
+        private String slug;
+        private String categoryName;
+        private boolean isProduct;
+        private ArrayList<String> categoriesArrayList = new ArrayList<>();
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getCompanyId() {
+            return companyId;
+        }
+
+        public void setCompanyId(String companyId) {
+            this.companyId = companyId;
+        }
+
+        public String getPoster() {
+            return poster;
+        }
+
+        public void setPoster(String poster) {
+            this.poster = poster;
+        }
+
+        public String getSpotlightPoster() {
+            return spotlightPoster;
+        }
+
+        public void setSpotlightPoster(String spotlightPoster) {
+            this.spotlightPoster = spotlightPoster;
+        }
+
+        public String getSlug() {
+            return slug;
+        }
+
+        public void setSlug(String slug) {
+            this.slug = slug;
+        }
+
+        public String getCategoryName() {
+            return categoryName;
+        }
+
+        public void setCategoryName(String categoryName) {
+            this.categoryName = categoryName;
+        }
+
+        public boolean isProduct() {
+            return isProduct;
+        }
+
+        public void setProduct(boolean product) {
+            isProduct = product;
+        }
+
+        public ArrayList<String> getCategoriesArrayList() {
+            return categoriesArrayList;
+        }
+
+        public void setCategoriesArrayList(ArrayList<String> categoriesArrayList) {
+            this.categoriesArrayList = categoriesArrayList;
+        }
     }
 }
