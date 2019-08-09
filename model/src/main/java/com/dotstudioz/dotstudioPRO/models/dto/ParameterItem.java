6cf3b20f2ce1d5d51f9730d404dcb28b0d03947c
Mohsin Shaikh <mohsin@dotstudioz.com>
2019-08-09 20:28:50 +0530

    Added few more data types to public ParameterItem

--- model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/ParameterItem.java
+++ model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/ParameterItem.java
@@ -8,10 +8,30 @@ public class ParameterItem {
 
     private String parameterName;
     private String parameterValue;
+    private int parameterIntValue;
+    private long parameterLongValue;
+    private double parameterDoubleValue;
+    private boolean parameterBooleanValue;
 
     public ParameterItem(String parameterName, String parameterValue) {
         setParameterName(parameterName);
         setParameterValue(parameterValue);
+    }
+    public ParameterItem(String parameterName, int parameterIntValue) {
+        setParameterName(parameterName);
+        setParameterIntValue(parameterIntValue);
+    }
+    public ParameterItem(String parameterName, long parameterLongValue) {
+        setParameterName(parameterName);
+        setParameterLongValue(parameterLongValue);
+    }
+    public ParameterItem(String parameterName, double parameterDoubleValue) {
+        setParameterName(parameterName);
+        setParameterDoubleValue(parameterDoubleValue);
+    }
+    public ParameterItem(String parameterName, boolean parameterBooleanValue) {
+        setParameterName(parameterName);
+        setParameterBooleanValue(parameterBooleanValue);
     }
 
     public String getParameterName() {
@@ -28,5 +48,37 @@ public class ParameterItem {
 
     public void setParameterValue(String parameterValue) {
         this.parameterValue = parameterValue;
+    }
+
+    public int getParameterIntValue() {
+        return parameterIntValue;
+    }
+
+    public void setParameterIntValue(int parameterIntValue) {
+        this.parameterIntValue = parameterIntValue;
+    }
+
+    public long getParameterLongValue() {
+        return parameterLongValue;
+    }
+
+    public void setParameterLongValue(long parameterLongValue) {
+        this.parameterLongValue = parameterLongValue;
+    }
+
+    public double getParameterDoubleValue() {
+        return parameterDoubleValue;
+    }
+
+    public void setParameterDoubleValue(double parameterDoubleValue) {
+        this.parameterDoubleValue = parameterDoubleValue;
+    }
+
+    public boolean isParameterBooleanValue() {
+        return parameterBooleanValue;
+    }
+
+    public void setParameterBooleanValue(boolean parameterBooleanValue) {
+        this.parameterBooleanValue = parameterBooleanValue;
     }
 }
