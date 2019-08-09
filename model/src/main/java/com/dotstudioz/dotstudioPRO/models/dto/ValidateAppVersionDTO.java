aa0fe5c1efb3c4ef7b02eca780bf62e5ecedcda3
Mohsin Shaikh <mohsin@dotstudioz.com>
2019-08-09 23:06:31 +0530

    Updated ValidateAppVersionDTO to include id and packageName

--- model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/ValidateAppVersionDTO.java
+++ model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/ValidateAppVersionDTO.java
@@ -4,11 +4,29 @@ package com.dotstudioz.dotstudioPRO.models.dto;
  * Created by user on 15-06-2016.
  */
 public class ValidateAppVersionDTO {
+    private String id;
     private String companyId;
+    private String packageName;
     private String platform;
     private String version;
     private String appStoreURL;
     private int severityLevel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
 
     public String getCompanyId() {
         return companyId;
