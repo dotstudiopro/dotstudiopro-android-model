9f369048b6989a5f2a7e5c203650c34c0c25517d
Mohsin Shaikh <mohsin@dotstudioz.com>
2020-07-08 01:06:59 +0530

    SpotLightChannelDTOForCollection for couple of SpotLightChannelDTO in same DTO

---
+++ model/src/main/java/com/dotstudioz/dotstudioPRO/models/dto/SpotLightChannelDTOForCollection.java
@@ -0,0 +1,23 @@
+package com.dotstudioz.dotstudioPRO.models.dto;
+
+public class SpotLightChannelDTOForCollection {
+
+    private SpotLightChannelDTO spotLightChannelDTO1 = new SpotLightChannelDTO();
+    private SpotLightChannelDTO spotLightChannelDTO2 = new SpotLightChannelDTO();
+
+    public SpotLightChannelDTO getSpotLightChannelDTO1() {
+        return spotLightChannelDTO1;
+    }
+
+    public void setSpotLightChannelDTO1(SpotLightChannelDTO spotLightChannelDTO1) {
+        this.spotLightChannelDTO1 = spotLightChannelDTO1;
+    }
+
+    public SpotLightChannelDTO getSpotLightChannelDTO2() {
+        return spotLightChannelDTO2;
+    }
+
+    public void setSpotLightChannelDTO2(SpotLightChannelDTO spotLightChannelDTO2) {
+        this.spotLightChannelDTO2 = spotLightChannelDTO2;
+    }
+}
